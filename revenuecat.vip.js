24bd91119566eac2c273b11feba8f7f2025e9685
Fromboy <93315528+Fromboy@users.noreply.github.com>
2026-07-28 12:20:21 +0800

    Update revenuecat.vip.js

--- revenuecat.vip.js
+++ revenuecat.vip.js
@@ -14,7 +14,7 @@ https://appraven.net/collection/77299969
 [rewrite_local]
 
 # ～ RevenueCat
-^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://ddgksf2013.top/scripts/revenuecat.vip.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/Fromboy/QxLoonShadowrocket/refs/heads/quantumult-x/revenuecat.vip.js
 ^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/Fromboy/QxLoonShadowrocket/refs/heads/quantumult-x/deleteHeader.js
 [mitm]
 
